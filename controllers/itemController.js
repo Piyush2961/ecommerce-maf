0a490b10e8be0d2791ed70ec454a05f429184a85
Piyush2961 <piyushkumar2961@gmail.com>
2022-03-07 21:17:11 +0530

    updateItem

--- controllers/itemController.js
+++ controllers/itemController.js
@@ -1,58 +1,92 @@
-const Item = require('./../models/itemModel')
+const { findByIdAndDelete } = require("./../models/itemModel");
+const Item = require("./../models/itemModel");
 
-exports.addItem=(req,res)=>{
-    const newItem = new Item(req.body)
-    newItem.save((err,item)=>{
-        if(err){
-            console.log(err.message)
-           return res.status(400).json({
-                error:err
-            })
-        }
-        res.status(200).json({
-            item
-        })
-    })
-}
+exports.addItem = async (req, res) => {
+  try {
+    const item = await Item.create(req.body);
+    res.status(201).json({
+      status: "success",
+      data: {
+        item,
+      },
+    });
+  } catch (err) {
+    res.status(400).json({
+      status: "fail",
+      message: err.message,
+    });
+  }
+};
 
-exports.getAllItems=(req,res)=>{
-    Item.find({},(err,items)=>{
-        if(err){
-            console.log(err.message)
-           return res.status(400).json({
-                error:err
-            })
-        }
-        res.status(200).json({
-            items
-        })
-    })
-}
+exports.getAllItems = async (req, res) => {
+  try {
+    const items = await Item.find();
+    res.status(200).json({
+      status: "success",
+      results: items.length,
+      data: {
+        items,
+      },
+    });
+  } catch (err) {
+    res.status(404).json({
+      status: "fail",
+      message: err.message,
+    });
+  }
+};
 
-exports.deleteItem=(req,res)=>{
-    Item.findByIdAndDelete(req.params.id,(err,item)=>{
-        if(err){
-            console.log(err.message)
-           return res.status(400).json({
-                error:err
-            })
-        }
-        res.status(200).json({
-            item
-        })
-    })
-}
+exports.getItem = async (req, res) => {
+  try {
+    const item = await Item.findById(req.params.id);
+    res.status(200).json({
+      status: "success",
+      data: {
+        item,
+      },
+    });
+  } catch (err) {
+    res.status(404).json({
+      status: "fail",
+      message: err.message,
+    });
+  }
+};
 
-exports.getItem=(req,res)=>{
-    Item.findById(req.params.id,(err,item)=>{
-        if(err){
-            console.log(err.message)
-           return res.status(400).json({
-                error:err
-            })
-        }
-        res.status(200).json({
-            item
-        })
-    })
-}
+exports.deleteItem = async (req, res) => {
+  try {
+    const item = await Item.findByIdAndDelete(req.params.id);
+
+    res.status(200).json({
+      status: "success",
+      data: {
+        item,
+      },
+    });
+  } catch (err) {
+    res.status(404).json({
+      status: "fail",
+      message: err.message,
+    });
+  }
+};
+
+exports.updateItem = async (req, res) => {
+  try {
+    const updatedItem = await Item.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+    });
+
+    res.status(200).json({
+      status: "success",
+      data: {
+        updatedItem,
+      },
+    });
+  } catch (err) {
+    res.status(404).json({
+      status: "fail",
+      message: err.message,
+    });
+  }
+};
--- routes/itemRoutes.js
+++ routes/itemRoutes.js
@@ -1,9 +1,13 @@
-const express = require('express')
+const express = require("express");
 
-const router = express.Router()
-const itemController = require('./../controllers/itemController')
+const router = express.Router();
+const itemController = require("./../controllers/itemController");
 
-router.route('/').post(itemController.addItem).get(itemController.getAllItems)
-router.route('/:id').get(itemController.getItem).delete(itemController.deleteItem)
+router.route("/").post(itemController.addItem).get(itemController.getAllItems);
+router
+  .route("/:id")
+  .get(itemController.getItem)
+  .delete(itemController.deleteItem)
+  .patch(itemController.updateItem);
 
-module.exports=router
+module.exports = router;
