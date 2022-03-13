7aa7a7aba16aed1205f77b96dcff1be0c57dc5f5
Piyush2961 <piyushkumar2961@gmail.com>
2022-03-13 22:52:30 +0530

    enable cors

--- app.js
+++ app.js
@@ -1,12 +1,16 @@
 const express = require("express");
 const compression = require('compression')
 const fileUpload = require('express-fileupload')
+const cors = require('cors')
 const app = express();
 app.use(express.json());
+
+app.use(cors())
 
 app.use(fileUpload({
     useTempFiles: true
 }))
+
 
 const itemRouter = require("./routes/itemRoutes");
 const userRouter = require('./routes/userRoutes');
